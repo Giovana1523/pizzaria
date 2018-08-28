93f683f27309e5f4495e44adfc657379dc168c5d
Giovana de Souza <giovana1523@gmail.com>
2018-08-28 09:12:18 -0300

    Base2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
 function enviar(){
-    alert(estafuncionando);
+
+    var nome = document.getElementById('nome').value;
+    alert(nome);
+    var idade = document.getElementById('idade').value;
+    alert(idade);
+    var sexo = document.getElementById('sexo').value;
+    alert(sexo);
+    var endereco = document.getElementById('endereco').value;
+    alert(endereco);
+    if (nome.length <=2 || nome.length>30) {
+        alert('Tamanho do nome invalido!');
+    }else if (idade<=0){
+        alert('Idade invalida!');
+
+    }
 }
 $(".btn-menu").click(function () {
     $(".menu").show();
